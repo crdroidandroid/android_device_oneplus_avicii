b52074f31afb842db4a17daaed7268efa21632cd
Puranjay Savar Mattas <puranjaysavarmattas@gmail.com>
2023-11-03 19:25:23 +0000

    avicii: fix android 14 sepolicy_vndr-legacy-um path change
    sepolicy_vndr-legacy-um => sepolicy_vndr/legacy-um

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -175,7 +175,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2023-03-05
 
 # SEPolicy
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy_vndr/legacy-um/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
 # Touch
